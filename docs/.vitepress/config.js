4c08dbc55c6f86c07e24677cdaf24ce69bc3ad86
李良贤 <liangxian.li@patpat.com>
2023-12-18 11:27:07 +0800

    feat: init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,66 @@
+
+
+module.exports = {
+  base: '/',
+  title: 'Hello VitePress',
+  description: 'Just playing around.',
+  lastUpdated: true,
+  themeConfig: {
+    smoothScroll: true,
+    logo: '/logo.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Config', link: '/config/' },
+      { text: 'External', link:  '' }
+    ],
+    sidebar: [
+      {
+        text: 'Guide',
+        collapsible: true,
+        items: [
+          {
+            text: 'default',
+            link: '/guide/index',
+            title: 'ff',
+          },
+          {
+            text: 'ff',
+            link: '/guide/ff',
+            title: 'ff',
+          }
+        ],
+      },
+      {
+        '/': [
+          {
+            title: 'Home',
+            collapsible: true,
+            link: '/'
+          },
+          {
+            title: 'about',
+            collapsible: true,
+            link: '/about'
+          }
+        ],
+      },
+      {
+        '/config/': [
+          {
+            title: 'Config',
+          }
+        ],
+      }
+    ],
+    // algolia: {
+    //   apiKey: '<API_KEY>',
+    //   indexName: '<INDEX_NAME>',
+    //   // 如果 Algolia 没有为你提供 `appId` ，使用 `BH4D9OD16A` 或者移除该配置项
+    //   appId: '<APP_ID>'
+    // },
+    // lastUpdated: 'Last Updated', // string | boolean
+    // search: true,
+    // searchMaxSuggestions: 10,
+  }
+}
---
+++ docs/guide/index.md
@@ -0,0 +1,4 @@
+# guide
+
+scvdsvk
+can be used to create a new project
